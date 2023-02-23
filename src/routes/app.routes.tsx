61c2ffa17474c4705ff5a630ded6f00fd52ca4d5
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-23 20:59:35 -0300

    customizing tab bottom navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,17 +1,61 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import {
+  BottomTabNavigationProp,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs'
 import { Exercise } from '@screens/exercise'
 import { History } from '@screens/history'
 import { Home } from '@screens/home'
 import { Profile } from '@screens/profile'
 
-const { Navigator, Screen } = createBottomTabNavigator()
+import HomeSvg from '@assets/home.svg'
+import HistorySvg from '@assets/history.svg'
+import ProfileSvg from '@assets/profile.svg'
+import { useTheme } from 'native-base'
+
+type AppRoutesProps = {
+  home: undefined
+  exercise: undefined
+  profile: undefined
+  history: undefined
+}
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutesProps>
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutesProps>()
 
 export function AppRoutes() {
+  const { sizes } = useTheme()
+  const iconSize = sizes[6]
+
   return (
-    <Navigator>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
   )
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 import { useTheme } from 'native-base'
-import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
 
 export function Routes() {
   const { colors } = useTheme()
@@ -9,7 +9,7 @@ export function Routes() {
   theme.colors.background = colors.gray['700']
   return (
     <NavigationContainer theme={theme}>
-      <AuthRoutes />
+      <AppRoutes />
     </NavigationContainer>
   )
 }
