895d3c3f67aad0ad574802957d2cee7c572c6a99
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-25 02:16:30 -0300

    finalizing exercise interface

--- src/screens/exercise.tsx
+++ src/screens/exercise.tsx
@@ -1,10 +1,21 @@
 import { useNavigation } from '@react-navigation/native'
 import { TouchableOpacity } from 'react-native'
-import { VStack, Icon, HStack, Heading, Text } from 'native-base'
+import {
+  VStack,
+  Icon,
+  HStack,
+  Heading,
+  Text,
+  Image,
+  Box,
+  Button,
+} from 'native-base'
 import { Feather } from '@expo/vector-icons'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
+import SeriesSvg from '@assets/series.svg'
 import BodySvg from '@assets/body.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -37,6 +48,45 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
+          }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
+
+        <Box bg="gray.600" rounded="md" pb={4} px={4}>
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 séries
+              </Text>
+            </HStack>
+
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+
+          <Button>Marcar como realizado</Button>
+        </Box>
       </VStack>
     </VStack>
   )
