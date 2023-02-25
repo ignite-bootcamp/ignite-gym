3a0d20e5b7d5e6cf6f0b02ff4adf530d928c25b4
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-24 23:08:10 -0300

    listing groups with flatlist

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,25 +1,37 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { HStack, VStack } from 'native-base'
+import { FlatList, VStack } from 'native-base'
 import { useState } from 'react'
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState('costa')
+  const [groups, setGroups] = useState([
+    'Costas',
+    'Bícepts',
+    'Tríceps',
+    'Ombro',
+  ])
   return (
     <VStack color="white" flex={1}>
       <HomeHeader />
-      <HStack>
-        <Group
-          name="Costas"
-          isActive={groupSelected === 'costa'}
-          onPress={() => setGroupSelected('costa')}
-        />
-        <Group
-          name="Ombro"
-          isActive={groupSelected === 'ombro'}
-          onPress={() => setGroupSelected('ombro')}
-        />
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{
+          px: 8,
+        }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
