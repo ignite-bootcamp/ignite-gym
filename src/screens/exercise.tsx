3ada676f5b0a443ad58176fa9afa65abed4891e8
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-25 02:19:29 -0300

    little adjustments

--- src/screens/exercise.tsx
+++ src/screens/exercise.tsx
@@ -9,6 +9,7 @@ import {
   Image,
   Box,
   Button,
+  ScrollView,
 } from 'native-base'
 import { Feather } from '@expo/vector-icons'
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
@@ -49,45 +50,46 @@ export function Exercise() {
           </HStack>
         </HStack>
       </VStack>
+      <ScrollView>
+        <VStack p={8}>
+          <Image
+            w="full"
+            h={80}
+            source={{
+              uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
+            }}
+            alt="Nome do exercício"
+            mb={3}
+            resizeMode="cover"
+            rounded="lg"
+          />
 
-      <VStack p={8}>
-        <Image
-          w="full"
-          h={80}
-          source={{
-            uri: 'http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
-          }}
-          alt="Nome do exercício"
-          mb={3}
-          resizeMode="cover"
-          rounded="lg"
-        />
+          <Box bg="gray.600" rounded="md" pb={4} px={4}>
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb={6}
+              mt={5}
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="gray.200" ml="2">
+                  3 séries
+                </Text>
+              </HStack>
 
-        <Box bg="gray.600" rounded="md" pb={4} px={4}>
-          <HStack
-            alignItems="center"
-            justifyContent="space-around"
-            mb={6}
-            mt={5}
-          >
-            <HStack>
-              <SeriesSvg />
-              <Text color="gray.200" ml="2">
-                3 séries
-              </Text>
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="gray.200" ml="2">
+                  12 repetições
+                </Text>
+              </HStack>
             </HStack>
 
-            <HStack>
-              <RepetitionsSvg />
-              <Text color="gray.200" ml="2">
-                12 repetições
-              </Text>
-            </HStack>
-          </HStack>
-
-          <Button>Marcar como realizado</Button>
-        </Box>
-      </VStack>
+            <Button>Marcar como realizado</Button>
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
