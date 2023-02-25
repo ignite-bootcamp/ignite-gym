e1683910dfe4cb5ea170aaa889320f64789fec99
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-25 01:52:04 -0300

    adding skeleton loading to profile picture

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,9 +1,32 @@
-import { Center, Text } from 'native-base'
+import { ScreenHeader } from '@components/ScreenHeader'
+import { Center, Image, Skeleton, VStack } from 'native-base'
+import { useState } from 'react'
+import { ScrollView } from 'react-native'
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
   return (
-    <Center color="white" flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              boxSize={33}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <Image
+              source={{ uri: 'https://github.com/guivictorr.png' }}
+              alt="Foto de usuário"
+              boxSize={33}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
