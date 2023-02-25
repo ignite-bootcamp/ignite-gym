6f3a5a87dc262736ed1b238897cdd56748c075c3
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-25 01:41:50 -0300

    list empty component for section list

--- src/screens/history.tsx
+++ src/screens/history.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Heading, SectionList, VStack } from 'native-base'
+import { Heading, SectionList, Text, VStack } from 'native-base'
 import { HistoryCard } from '@components/HistoryCard'
 import { ScreenHeader } from '@components/ScreenHeader'
 
@@ -28,6 +28,15 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+        }
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
       />
     </VStack>
   )
